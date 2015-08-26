a5836654726179440bd7477983d548242e6f108d
universefei <826171850@qq.com>
2015-08-26 22:03:13 +0800

    add ubuntu 163 apt source

--- config/pkgs/apt.sh
+++ config/pkgs/apt.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
 
+sudo mv /etc/apt/sources.list /etc/apt/sources.list.bak
+sudo cp sources.list /etc/apt/sources.list
+sudo apt-get update
 
 pkgs='
 cloc subversion axel xsel zsh tmux vim ctags git g++ tree python tig curl
